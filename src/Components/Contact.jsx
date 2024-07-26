4aa795446611d652d9c6b136a1dad990ada94e6e
anishsaini <anishoficial@gmail.com>
2024-07-26 19:34:11 +0530

    failed to send email it can't find user id

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,8 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+// import styled from "styled-components";
+
+// npm i @emailjs/browser
 
 const Contact = () => {
   const form = useRef();
@@ -8,15 +11,19 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm("", "", form.current, {
-        publicKey: "",
-      })
+      .sendForm(
+        "service_ufl4dld",
+        "template_omx5yh1",
+        form.current,
+        "50rnZvF5Y5y7WgDH2mKoe"
+      )
       .then(
-        () => {
-          console.log("SUCCESS!");
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
         },
         (error) => {
-          console.log("FAILED...", error.text);
+          console.log(error.text);
         }
       );
   };
@@ -35,3 +42,57 @@ const Contact = () => {
 };
 
 export default Contact;
+
+// Styles
+// const StyledContactForm = styled.div`
+//   width: 400px;
+
+//   form {
+//     display: flex;
+//     align-items: flex-start;
+//     flex-direction: column;
+//     width: 100%;
+//     font-size: 16px;
+
+//     input {
+//       width: 100%;
+//       height: 35px;
+//       padding: 7px;
+//       outline: none;
+//       border-radius: 5px;
+//       border: 1px solid rgb(220, 220, 220);
+
+//       &:focus {
+//         border: 2px solid rgba(0, 206, 158, 1);
+//       }
+//     }
+
+//     textarea {
+//       max-width: 100%;
+//       min-width: 100%;
+//       width: 100%;
+//       max-height: 100px;
+//       min-height: 100px;
+//       padding: 7px;
+//       outline: none;
+//       border-radius: 5px;
+//       border: 1px solid rgb(220, 220, 220);
+
+//       &:focus {
+//         border: 2px solid rgba(0, 206, 158, 1);
+//       }
+//     }
+
+//     label {
+//       margin-top: 1rem;
+//     }
+
+//     input[type="submit"] {
+//       margin-top: 2rem;
+//       cursor: pointer;
+//       background: rgb(249, 105, 14);
+//       color: white;
+//       border: none;
+//     }
+//   }
+// `;
