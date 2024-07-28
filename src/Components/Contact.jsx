9675f4cf1b3e3ac25946b91547bb252013762e9d
anishsaini <anishoficial@gmail.com>
2024-07-28 10:04:54 +0530

    fill all errors now it work properly

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,6 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
-// import styled from "styled-components";
-
-// npm i @emailjs/browser
+import styled from "styled-components";
 
 const Contact = () => {
   const form = useRef();
@@ -15,7 +13,7 @@ const Contact = () => {
         "service_ufl4dld",
         "template_omx5yh1",
         form.current,
-        "50rnZvF5Y5y7WgDH2mKoe"
+        "Oi99c0euQUrqk_0Xd"
       )
       .then(
         (result) => {
@@ -29,70 +27,72 @@ const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <StyledContactForm>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </StyledContactForm>
   );
 };
 
 export default Contact;
 
 // Styles
-// const StyledContactForm = styled.div`
-//   width: 400px;
+const StyledContactForm = styled.div`
+  width: 400px;
 
-//   form {
-//     display: flex;
-//     align-items: flex-start;
-//     flex-direction: column;
-//     width: 100%;
-//     font-size: 16px;
+  form {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    font-size: 16px;
 
-//     input {
-//       width: 100%;
-//       height: 35px;
-//       padding: 7px;
-//       outline: none;
-//       border-radius: 5px;
-//       border: 1px solid rgb(220, 220, 220);
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
 
-//       &:focus {
-//         border: 2px solid rgba(0, 206, 158, 1);
-//       }
-//     }
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
 
-//     textarea {
-//       max-width: 100%;
-//       min-width: 100%;
-//       width: 100%;
-//       max-height: 100px;
-//       min-height: 100px;
-//       padding: 7px;
-//       outline: none;
-//       border-radius: 5px;
-//       border: 1px solid rgb(220, 220, 220);
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
 
-//       &:focus {
-//         border: 2px solid rgba(0, 206, 158, 1);
-//       }
-//     }
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
 
-//     label {
-//       margin-top: 1rem;
-//     }
+    label {
+      margin-top: 1rem;
+    }
 
-//     input[type="submit"] {
-//       margin-top: 2rem;
-//       cursor: pointer;
-//       background: rgb(249, 105, 14);
-//       color: white;
-//       border: none;
-//     }
-//   }
-// `;
+    input[type="submit"] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: rgb(249, 105, 14);
+      color: white;
+      border: none;
+    }
+  }
+`;
